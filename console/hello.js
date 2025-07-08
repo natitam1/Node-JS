15c7720b19200360f0036ec5a6acc555a12652e8
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-08 23:22:06 +0300

    Update the hello js

--- console/hello.js
+++ console/hello.js
@@ -1,5 +1,18 @@
 // Fetch name from command argument, environment, or fallback
-const nameArg =
-  process.argv[2] || process.env.USER || process.env.USERNAME || "world";
 
-console.log(`Hello, ${nameArg}`);
+const nameArg = capitalize(
+  process.argv[2] || process.env.USER || process.env.USERNAME || "world"
+);
+
+// output message
+
+console.log(`Hello, ${nameArg}!`);
+
+function capitalize(str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
